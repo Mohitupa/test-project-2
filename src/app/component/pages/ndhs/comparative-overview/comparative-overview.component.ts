0ca5405dbc4c9f4306910749aa895cef5e264246
jp-mangoit <jp@mangoitsolutions.com>
2022-08-19 17:45:12 +0530

    NDHS MAP complete

--- src/app/component/pages/ndhs/comparative-overview/comparative-overview.component.ts
+++ src/app/component/pages/ndhs/comparative-overview/comparative-overview.component.ts
@@ -1,11 +1,89 @@
-import {Component, OnInit} from '@angular/core';
+import {AfterViewInit, Component, OnInit} from '@angular/core';
+import * as echarts from 'echarts';
+import * as $ from "jquery";
 
 @Component({
     selector: 'app-comparative-overview',
     templateUrl: './comparative-overview.component.html',
     styleUrls: ['./comparative-overview.component.css'],
 })
-export class ComparativeOverviewComponent implements OnInit {
+export class ComparativeOverviewComponent implements OnInit,AfterViewInit {
+    
     ngOnInit() {
     }
+
+    ngAfterViewInit(): void {
+        this.comparativeResultNetworkChart()
+    }
+
+    comparativeResultNetworkChart() {
+        type EChartsOption = echarts.EChartsOption;
+    
+        var chartDom = document.getElementById('main15')!;
+        var myChart = echarts.init(chartDom);
+        var option: EChartsOption;
+    
+        interface GraphNode {
+          symbolSize: number;
+          label?: {
+            show?: boolean;
+          };
+        }
+    
+        myChart.showLoading();
+        $.getJSON('../../assets/data/network.json', function (graph: any) {
+    
+          myChart.hideLoading();
+    
+          graph.nodes.forEach(function (node: GraphNode) {
+            node.label = {
+              show: node.symbolSize > 30
+            };
+          });
+          option = {
+            title: {
+              text: '',
+              subtext: '',
+              top: 'bottom',
+              left: 'right'
+            },
+            tooltip: {
+              trigger: 'item',
+              formatter: function (params: any) {
+                  if (params.data.name) {
+                      return params.name;
+                  }
+                  return;
+              },
+          },
+            series: [
+              {
+                name: '',
+                type: 'graph',
+                layout: 'none',
+                data: graph.nodes,
+                links: graph.links,
+                categories: graph.categories,
+                roam: true,
+                label: {
+                  color: '#fff',
+                  position: 'inside',
+                  align: 'center',
+                  formatter: '{b}',
+                  verticalAlign: 'middle',
+                  fontSize: '10'
+                },
+                lineStyle: {
+                  color: 'source',
+                  curveness: 0.3
+                },
+              }
+            ]
+          };
+    
+    
+    
+          myChart.setOption(option);
+        });
+      }
 }
--- src/app/component/pages/ndhs/countries/ndhs-countries/ndhs-countries.component.ts
+++ src/app/component/pages/ndhs/countries/ndhs-countries/ndhs-countries.component.ts
@@ -1,12 +1,27 @@
-import {Component} from '@angular/core';
-
-
+import {Component, OnInit} from '@angular/core';
+import { ApiDataService } from 'src/app/services/api-data.service';
 
 @Component({
     selector: 'app-ndhs-countries',
     templateUrl: './ndhs-countries.component.html',
     styleUrls: ['./ndhs-countries.component.css'],
 })
-export class NdhsCountriesComponent {
-  
+export class NdhsCountriesComponent implements OnInit {
+    data2021: any;
+    data2022: any;
+    countries: any;
+    constructor(private apiData: ApiDataService) {
+     }
+
+     ngOnInit(): void {
+        this.apiData.getCountriesData().subscribe((data) => {
+            this.data2021 = data[2021];
+            this.data2022 = data[2022];
+            console.log(this.data2021);
+        })
+
+        setTimeout(() => {
+            this.countries = this.data2021;
+        }, 2000);
+     }
 }
