f58d7593f911fc2b6c5695a3710bfe93b835813e
jp-mangoit <jp@mangoitsolutions.com>
2022-08-24 11:42:46 +0530

    minor changes

--- src/app/services/local-data.service.ts
+++ src/app/services/local-data.service.ts
@@ -5,32 +5,9 @@ import { Subject, BehaviorSubject } from 'rxjs';
   providedIn: 'root'
 })
 export class LocalDataService {
-  
   showHeaderMenu = new Subject<boolean>();
-
-  yearSource = new BehaviorSubject<string>('2021');
-  currentYear = this.yearSource.asObservable();
-
   governanceTypeSource = new BehaviorSubject<any>('1');
   currentGovernanceType = this.governanceTypeSource.asObservable();
-
-  countrySource = new BehaviorSubject<string>('44');
-  currentCountry = this.countrySource.asObservable();
-
   selectedYear:any = ['2021'];
-
   mapSelectedCountry:any = "Australia";
-
-
-  changeYear(message: string) {
-      this.yearSource.next(message);
-  }
-
-  changeGovernanceType(message:any) {
-      this.governanceTypeSource.next(message);
-  }
-
-  changeCountryType(message:any) {
-      this.countrySource.next(message);
-  }
 }
